10c78bab611c823677cc79d3d20627abe20956b0
bigtran <leouzdc@qq.com>
2020-10-01 02:37:42 +0800

    修改模板文件夹问题

---
+++ public/theme/default/inc/header.php
@@ -0,0 +1,19 @@
+        <title><?=_siteInfo('title')?></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta name="description" content="<?=_siteInfo('description')?>" />
+        <meta name="keywords" content="<?=_siteInfo('keywords')?>" />
+        <meta name="author" content="<?=_siteInfo('author')?>" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+        <link rel="shortcut icon" href="<?=_themeUrl()?>images/favicon.ico" />        
+        <link rel="stylesheet" type="text/css"  href='<?=_themeUrl()?>css/clear.css' />
+        <link rel="stylesheet" type="text/css"  href='<?=_themeUrl()?>css/common.css' />
+        <link rel="stylesheet" type="text/css"  href='<?=_themeUrl()?>css/font-awesome.min.css' />
+        <link rel="stylesheet" type="text/css"  href='<?=_themeUrl()?>css/flickity.min.css' />
+        <link rel="stylesheet" type="text/css"  href='<?=_themeUrl()?>css/sm-clean.css' />        
+        <link rel="stylesheet" type="text/css"  href='<?=_themeUrl()?>style.css' />
+
+        <!--[if lt IE 9]>
+                <script src="<?=$theme_url?>js/html5.js"></script>            
+                <script src="<?=$theme_url?>js/respond.min.js"></script>                   
+        <![endif]-->    
---
+++ public/theme/default/inc/sidebar.php
@@ -0,0 +1,31 @@
+                <div class="content-left-holder">
+                    <div class="welcome-content">
+                        <img class="welcome-image" src="<?=$theme_url?>images/background.png" alt="" />
+                        <h1 class="site-logo" style="height:350px; font-size:42px; line-height: 42px;">
+                            <a href="/">
+                                <?=_siteInfo('title')?>
+                            </a>
+                        </h1>
+                        <div class="site-description">
+                            <iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=280 height=52 src="//music.163.com/outchain/player?type=2&id=26771173&auto=0&height=32" style="margin:-10px 0 10px -10px"></iframe>
+                            <br>
+                            bigtran,<br>
+                            at Beijing, China.<br>
+                            contact via <a href="mailto:bigtran@qq.com">bigtran@qq.com</a>
+                            <br><br><br>
+                            <a href="<?=_siteInfo('baseURL')?>/achieves" target="_blank">文章归档</a><br>
+                            <a href="<?=_siteInfo('baseURL')?>/page/projects" target="_blank">一些在做的项目</a><br>
+                            <a href="<?=_siteInfo('baseURL')?>/page/about" target="_blank">关于我</a><br>
+                            <a href="<?=_siteInfo('baseURL')?>/rss" target="_blank">RSS</a>
+                            <br>
+                            <br>
+                            <br>
+                            <br>
+                            
+                            Powered by <a href="https://github.com/bigtran/translog">translog</a>, <br>
+                            Theme by <a href="https://github.com/bigtran/ThreeLittleBirds">threelittlebirds</a>.<br><br>
+
+                            &copy; <?=_siteInfo('copyright')?><br>
+                        </div>
+                    </div>
+                </div>
